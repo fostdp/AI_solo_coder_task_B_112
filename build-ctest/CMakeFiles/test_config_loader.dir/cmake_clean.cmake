eae08452165d2cbcec646a4f71f263a10e5a88a7
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-14 17:34:51 +0800

    Update project

---
+++ build-ctest/CMakeFiles/test_config_loader.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_config_loader.dir/backend/src/modules/config_loader.cpp.obj"
+  "CMakeFiles/test_config_loader.dir/backend/src/modules/config_loader.cpp.obj.d"
+  "CMakeFiles/test_config_loader.dir/tests/test_config_loader.cpp.obj"
+  "CMakeFiles/test_config_loader.dir/tests/test_config_loader.cpp.obj.d"
+  "libtest_config_loader.dll.a"
+  "test_config_loader.exe"
+  "test_config_loader.exe.manifest"
+  "test_config_loader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_config_loader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
