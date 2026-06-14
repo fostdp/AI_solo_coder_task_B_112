ea89b2972fa7e9ebe00105974712bc2570ca6b87
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-14 17:35:46 +0800

    Update project

---
+++ build-ctest/CMakeFiles/test_message_bus.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_message_bus.dir/tests/test_message_bus.cpp.obj"
+  "CMakeFiles/test_message_bus.dir/tests/test_message_bus.cpp.obj.d"
+  "libtest_message_bus.dll.a"
+  "test_message_bus.exe"
+  "test_message_bus.exe.manifest"
+  "test_message_bus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_message_bus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
