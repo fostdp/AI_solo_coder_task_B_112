0b6c4e890495b4330ec4dcfe08d7eeb03716c349
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-14 17:36:08 +0800

    Update project

---
+++ build-ctest/CMakeFiles/test_mold_engine.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_mold_engine.dir/backend/src/modules/mold_engine.cpp.obj"
+  "CMakeFiles/test_mold_engine.dir/backend/src/modules/mold_engine.cpp.obj.d"
+  "CMakeFiles/test_mold_engine.dir/tests/stub_clickhouse.cpp.obj"
+  "CMakeFiles/test_mold_engine.dir/tests/stub_clickhouse.cpp.obj.d"
+  "CMakeFiles/test_mold_engine.dir/tests/test_mold_engine.cpp.obj"
+  "CMakeFiles/test_mold_engine.dir/tests/test_mold_engine.cpp.obj.d"
+  "libtest_mold_engine.dll.a"
+  "test_mold_engine.exe"
+  "test_mold_engine.exe.manifest"
+  "test_mold_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_mold_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
