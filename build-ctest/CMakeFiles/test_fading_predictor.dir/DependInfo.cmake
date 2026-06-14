2c94b96e7f79fcea2c7be3fc4045c04721021c48
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-14 17:35:24 +0800

    Update project

---
+++ build-ctest/CMakeFiles/test_fading_predictor.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/SOLO-2/AI_solo_coder_task_A_112/backend/src/modules/fading_predictor.cpp" "CMakeFiles/test_fading_predictor.dir/backend/src/modules/fading_predictor.cpp.obj" "gcc" "CMakeFiles/test_fading_predictor.dir/backend/src/modules/fading_predictor.cpp.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_A_112/tests/stub_clickhouse.cpp" "CMakeFiles/test_fading_predictor.dir/tests/stub_clickhouse.cpp.obj" "gcc" "CMakeFiles/test_fading_predictor.dir/tests/stub_clickhouse.cpp.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_A_112/tests/test_fading_predictor.cpp" "CMakeFiles/test_fading_predictor.dir/tests/test_fading_predictor.cpp.obj" "gcc" "CMakeFiles/test_fading_predictor.dir/tests/test_fading_predictor.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
