4c975651833b8c85099073b93b53fb8a25df2dfb
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-14 17:34:10 +0800

    Update project

---
+++ build-ctest/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: D:/SOLO-2/AI_solo_coder_task_A_112
+# Build directory: D:/SOLO-2/AI_solo_coder_task_A_112/build-ctest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_fading_predictor "D:/SOLO-2/AI_solo_coder_task_A_112/build-ctest/test_fading_predictor.exe")
+set_tests_properties(test_fading_predictor PROPERTIES  _BACKTRACE_TRIPLES "D:/SOLO-2/AI_solo_coder_task_A_112/CMakeLists.txt;18;add_test;D:/SOLO-2/AI_solo_coder_task_A_112/CMakeLists.txt;0;")
+add_test(test_mold_engine "D:/SOLO-2/AI_solo_coder_task_A_112/build-ctest/test_mold_engine.exe")
+set_tests_properties(test_mold_engine PROPERTIES  _BACKTRACE_TRIPLES "D:/SOLO-2/AI_solo_coder_task_A_112/CMakeLists.txt;26;add_test;D:/SOLO-2/AI_solo_coder_task_A_112/CMakeLists.txt;0;")
+add_test(test_config_loader "D:/SOLO-2/AI_solo_coder_task_A_112/build-ctest/test_config_loader.exe")
+set_tests_properties(test_config_loader PROPERTIES  _BACKTRACE_TRIPLES "D:/SOLO-2/AI_solo_coder_task_A_112/CMakeLists.txt;32;add_test;D:/SOLO-2/AI_solo_coder_task_A_112/CMakeLists.txt;0;")
+add_test(test_message_bus "D:/SOLO-2/AI_solo_coder_task_A_112/build-ctest/test_message_bus.exe")
+set_tests_properties(test_message_bus PROPERTIES  _BACKTRACE_TRIPLES "D:/SOLO-2/AI_solo_coder_task_A_112/CMakeLists.txt;37;add_test;D:/SOLO-2/AI_solo_coder_task_A_112/CMakeLists.txt;0;")
