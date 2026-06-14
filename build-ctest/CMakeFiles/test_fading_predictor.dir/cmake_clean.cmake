8f4b316e7e69616eb330e8fdd667c49e8fc0dd8b
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-14 17:35:17 +0800

    Update project

---
+++ build-ctest/CMakeFiles/test_fading_predictor.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_fading_predictor.dir/backend/src/modules/fading_predictor.cpp.obj"
+  "CMakeFiles/test_fading_predictor.dir/backend/src/modules/fading_predictor.cpp.obj.d"
+  "CMakeFiles/test_fading_predictor.dir/tests/stub_clickhouse.cpp.obj"
+  "CMakeFiles/test_fading_predictor.dir/tests/stub_clickhouse.cpp.obj.d"
+  "CMakeFiles/test_fading_predictor.dir/tests/test_fading_predictor.cpp.obj"
+  "CMakeFiles/test_fading_predictor.dir/tests/test_fading_predictor.cpp.obj.d"
+  "libtest_fading_predictor.dll.a"
+  "test_fading_predictor.exe"
+  "test_fading_predictor.exe.manifest"
+  "test_fading_predictor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_fading_predictor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
