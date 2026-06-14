be673d87db976bb149d9da72459c123bbb51fad8
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-14 17:34:58 +0800

    Update project

---
+++ build-ctest/CMakeFiles/test_config_loader.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/SOLO-2/AI_solo_coder_task_A_112/backend/src/modules/config_loader.cpp" "CMakeFiles/test_config_loader.dir/backend/src/modules/config_loader.cpp.obj" "gcc" "CMakeFiles/test_config_loader.dir/backend/src/modules/config_loader.cpp.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_A_112/tests/test_config_loader.cpp" "CMakeFiles/test_config_loader.dir/tests/test_config_loader.cpp.obj" "gcc" "CMakeFiles/test_config_loader.dir/tests/test_config_loader.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
